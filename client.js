3113b448996e3e61151d703934048b5f5faf65e3
Dev Jana <dev@primeacademy.io>
2019-04-12 11:16:48 -0500

    user input captured

--- client.js
+++ client.js
@@ -1,5 +1,20 @@
+// out inventory array
+let inventory = [];
+
 $( document ).ready( readyNow );
 
+function addItem(){
+    console.log( 'in addItem' );
+    // get user input
+    console.log( $( '#sizeIn' ).val() );
+    console.log( $( '#colorIn' ).val() );
+    console.log( $( '#descriptionIn' ).val() );
+    // create a new object literal with the user input
+    // push the object into our inventory array
+    // display inventory on DOM
+    console.log( 'inventory:', inventory );
+} // end addItem
+
 function readyNow(){
-    console.log( 'JQ' );
-}
+    $( '#addItemButton' ).on( 'click', addItem );
+} // end readyNow
