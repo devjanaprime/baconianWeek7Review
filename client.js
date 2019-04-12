0e472142825562cac74e2d13e415f6c3a8e1ced0
Dev Jana <dev@primeacademy.io>
2019-04-12 11:40:42 -0500

    inventory showing on DOM

--- client.js
+++ client.js
@@ -15,9 +15,25 @@ function addItem(){
     // push the object into our inventory array
     inventory.push( newItem );
     // display inventory on DOM
-    console.log( 'inventory:', inventory );
+    displayInventory();
 } // end addItem
+
+function displayInventory(){
+    console.log( 'in displayInventory' );
+    // target ul element
+    let el = $( '#inventoryOut' );
+    el.empty();
+    // loop through the inventory
+    for( let i = 0; i<inventory.length; i++){
+        // display each item as a li: <li>SIZE, COLOR: DESCRIPTION</li>
+        const listItem = `<li>${ inventory[i].size },
+            ${ inventory[i].color }: 
+            ${ inventory[i].description }</li>`;
+        el.append( listItem );
+    }
+}
 
 function readyNow(){
     $( '#addItemButton' ).on( 'click', addItem );
+    displayInventory();
 } // end readyNow
