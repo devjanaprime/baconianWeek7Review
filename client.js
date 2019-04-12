907721e3170976e6184e46cc490a5442d3b46770
Dev Jana <dev@primeacademy.io>
2019-04-12 12:03:16 -0500

    end of lecture

--- client.js
+++ client.js
@@ -15,25 +15,46 @@ function addItem(){
     // push the object into our inventory array
     inventory.push( newItem );
     // display inventory on DOM
-    displayInventory();
+    displayInventory( inventory );
 } // end addItem
 
-function displayInventory(){
+function displayInventory( arrayToShow ){
     console.log( 'in displayInventory' );
     // target ul element
     let el = $( '#inventoryOut' );
     el.empty();
     // loop through the inventory
-    for( let i = 0; i<inventory.length; i++){
+    for( let i = 0; i<arrayToShow.length; i++){
         // display each item as a li: <li>SIZE, COLOR: DESCRIPTION</li>
-        const listItem = `<li>${ inventory[i].size },
-            ${ inventory[i].color }: 
-            ${ inventory[i].description }</li>`;
+        const listItem = `<li>${ arrayToShow[i].size },
+            ${ arrayToShow[i].color }: 
+            ${ arrayToShow[i].description }</li>`;
         el.append( listItem );
     }
 }
 
+function filter(){
+    console.log( 'in filter' );
+    let matches = [];
+    // get user input
+    const filterParameters = {
+        size: $( '#sizeFilterIn' ).val(),
+        color: $( '#colorFilterIn' ).val()
+    }
+    // loop through inventory and check for matches
+    for( let i =0; i<inventory.length; i++){
+        // push matches into an array
+        if( inventory[ i ].size === filterParameters.size 
+            && inventory[ i ].color === filterParameters.color ){
+            matches.push( inventory[ i ] );
+        } //end match check
+    } // end for
+    // update DOM with only matches
+    displayInventory( matches );
+}
+
 function readyNow(){
     $( '#addItemButton' ).on( 'click', addItem );
-    displayInventory();
+    $( '#filterButton' ).on( 'click', filter );
+    displayInventory( inventory );
 } // end readyNow
